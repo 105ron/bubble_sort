bb96752303055f42581f419a9a9b8741e388eab5
rhys <rbrooker105@gmail.com>
2016-08-08 20:40:30 +1000

    Completed bubble_sort_by method with yield

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,9 +11,31 @@ def bubble_sort(number_array)
     end
 	array_length -= 1 #Don't iterate over elements that are sorted
   end
-  puts number_array
+  number_array
 end
 
+puts bubble_sort([14,1,2,3,4,5,6,7,8,9,10,0])
 
+def bubble_sort_by(array_sort)
+  array_length = array_sort.length - 1 
+  sort = false
+  until  sort == true 
+	sort = true
+    for i in (0...array_length)
+      y = yield(array_sort[i],array_sort[i + 1]) #Our yield compares the two array elements
+	  if y > 0
+	    array_sort[i],array_sort[i + 1] = array_sort[i + 1],array_sort[i]
+        sort = false
+	  end
+    end
+    array_length -= 1 #Don't iterate over elements that are sorted
+  end
+  array_sort
+end
 
-bubble_sort([14,1,2,3,4,5,6,7,8,9,10,0])
+new_var = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
+puts new_var
+
+    
